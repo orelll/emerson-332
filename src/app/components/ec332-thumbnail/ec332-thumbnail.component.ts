b13d4c13d794b8a7dcdba816def676123eb5efd1
orelll <orlikszym@gmail.com>
2022-01-30 16:33:10 +0100

    part of upgrades

--- src/app/components/ec332-thumbnail/ec332-thumbnail.component.ts
+++ src/app/components/ec332-thumbnail/ec332-thumbnail.component.ts
@@ -1,6 +1,10 @@
-import { AfterContentChecked, AfterViewInit, Component, Input, OnInit } from '@angular/core';
-import { PagesName } from 'src/app/pages-name.enum';
+import { AfterViewInit, Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
+import { PagesName } from 'src/app/common/pages-name.enum';
+import { ThumbnailData } from 'src/app/common/thumbnail-data';
 import { HttpCallerService } from 'src/app/services/http-caller.service';
+import * as _ from 'lodash';
+import { TagReadingService } from 'src/app/services/tag-reading.service';
+import { element } from 'protractor';
 
 @Component({
   selector: 'app-ec332-thumbnail',
@@ -10,30 +14,65 @@ import { HttpCallerService } from 'src/app/services/http-caller.service';
 export class Ec332ThumbnailComponent implements OnInit, AfterViewInit {
 
   @Input() address = '';
+  @ViewChild('anlgcfContainer', {static:false}) anlgcfContainer2 : ElementRef;
 
-  private dataContainer: HTMLElement = undefined;
+  private anlgcfContainer: HTMLElement = undefined;
+  private dataBackup: ThumbnailData = undefined;
+  private currentData: ThumbnailData = undefined;
 
-  constructor(private httpCaller: HttpCallerService) {
+  constructor(private httpCaller: HttpCallerService,
+    private tagReader:TagReadingService) {
 
   }
+
   ngAfterViewInit(): void {
-    this.dataContainer = document.getElementById('hujsko');
+    this.anlgcfContainer = document.getElementById('anlgcfContainer');
     this.refreshCOmponentData();
+
   }
 
   ngOnInit(): void {
   }
 
-refreshCOmponentData():void{
-  this.httpCaller.loadPage(this.address, PagesName.tcp_ip).subscribe(body => {
-    this.dataContainer.innerHTML = body;
-    this.check();
-});
-}
+  refreshCOmponentData(): void {
+    this.httpCaller.loadPage(this.address, PagesName.anlgcf).subscribe(body => {
+      const containerDiv = this.anlgcfContainer2.nativeElement as HTMLDivElement;
+      containerDiv.innerHTML = body;
+      containerDiv.find
+      this.check();
+    });
+  }
 
-  check():void{
-    var doc = document.getElementsByName('ncitc03');
-    var tag = doc[0] as HTMLInputElement;
-    var tagValue = tag.value;
+  check(): void {
+
+    //fan ncian23
+    const fan = this.tagReader.getTagValue('ncian23');
+
+    //alarm
+    const alarm = this.tagReader.getTagValue('ncian20');
+
+    //alarm
+    const alsarm = this.tagReader.getTagValue('ncian20');
+
+    this.dataBackup = {
+      temperatureSet: 0.2,
+      fan: this.toBoolean(fan),
+      cooling: false,
+      defrosting: true,
+      alarm: this.toBoolean(alarm)
+    }
+
+    this.currentData = _.cloneDeep(this.dataBackup);
+  }
+
+  private toBoolean(value: any): boolean{
+    switch(value){
+      case "1":
+        return true;
+      case "0":
+          return false;
+      default:
+        return undefined;
+    }
   }
 }
