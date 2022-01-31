dd86d42d649ff540882de0646f74ad9d85dec8de
orelll <orlikszym@gmail.com>
2022-01-31 12:30:28 +0100

    added posting data

--- src/app/components/ec332-thumbnail/ec332-thumbnail.component.ts
+++ src/app/components/ec332-thumbnail/ec332-thumbnail.component.ts
@@ -14,32 +14,38 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 export class Ec332ThumbnailComponent implements OnInit, AfterViewInit {
 
   @Input() address = '';
-  @ViewChild('anlgcfContainer', {static:false}) anlgcfContainer2 : ElementRef;
+  @ViewChild('anlgcfContainer', { static: false }) anlgcfContainer2: ElementRef;
 
   private idSet: string = undefined;
   private anlgcfContainer: HTMLElement = undefined;
   private dataBackup: ThumbnailData = undefined;
 
+  private temperatureSetCode: string = 'ncian23';//TODO: to find
+  private fanCode: string = 'ncian23';
+  private coolingCode: string = 'ncian23';//
+  private defrostingCode: string = 'ncian23';//
+  private alarmCode: string = 'ncian20';
+
   dataForm: FormGroup = undefined;
-  public get enableSave(): boolean{
+  public get enableSave(): boolean {
     const thisSame = this.dataBackup?.temperatureSet !== this.dataForm.controls['temperatureSet'].value
-    || this.dataBackup?.fan !== this.dataForm.controls['fan'].value
-    || this.dataBackup?.cooling !== this.dataForm.controls['cooling'].value
-    || this.dataBackup?.defrosting !== this.dataForm.controls['defrosting'].value
-    || this.dataBackup?.alarm !== this.dataForm.controls['alarm'].value
-    ;
+      || this.dataBackup?.fan !== this.dataForm.controls['fan'].value
+      || this.dataBackup?.cooling !== this.dataForm.controls['cooling'].value
+      || this.dataBackup?.defrosting !== this.dataForm.controls['defrosting'].value
+      || this.dataBackup?.alarm !== this.dataForm.controls['alarm'].value
+      ;
     return !thisSame;
   }
 
   constructor(private httpCaller: HttpCallerService,
-    private tagReader:TagReadingService) {
-      this.dataForm = new FormGroup({
-        temperatureSet: new FormControl(0),
-        fan: new FormControl(false),
-        cooling: new FormControl(false),
-        defrosting: new FormControl(false),
-        alarm: new FormControl(false),
-      });
+    private tagReader: TagReadingService) {
+    this.dataForm = new FormGroup({
+      temperatureSet: new FormControl(0),
+      fan: new FormControl(false),
+      cooling: new FormControl(false),
+      defrosting: new FormControl(false),
+      alarm: new FormControl(false),
+    });
   }
 
   ngAfterViewInit(): void {
@@ -52,7 +58,7 @@ export class Ec332ThumbnailComponent implements OnInit, AfterViewInit {
 
   refreshCOmponentData(): void {
     const containerDiv = this.anlgcfContainer2.nativeElement as HTMLDivElement;
-    this.idSet = `x_${this.address.replace(/\./g, '_')}`;//this.address.replace(/\./g, '_')
+    this.idSet = `x_${this.address.replace(/\./g, '_')}`;
     containerDiv.id = this.idSet;
 
     this.httpCaller.loadPage(this.address, PagesName.anlgcf).subscribe(body => {
@@ -64,10 +70,10 @@ export class Ec332ThumbnailComponent implements OnInit, AfterViewInit {
   check(): void {
 
     //fan ncian23
-    const fan = this.tagReader.getTagValue(this.idSet, 'ncian23');
+    const fan = this.tagReader.getTagValue(this.idSet, this.fanCode);
 
     //alarm
-    const alarm = this.tagReader.getTagValue(this.idSet, 'ncian20');
+    const alarm = this.tagReader.getTagValue(this.idSet, this.alarmCode);
 
     //alarm
     const alarmx = this.tagReader.getTagValue(this.idSet, 'ncian20');
@@ -87,18 +93,41 @@ export class Ec332ThumbnailComponent implements OnInit, AfterViewInit {
     this.dataForm.controls['alarm'].setValue(this.dataBackup.alarm);
   }
 
-  onFormSubmit():void{
-
+  onFormSubmit(): void {
+    var changes = this.listChangedProperties();
+    this.httpCaller.postChange(this.address, PagesName.anlgcf, changes);
   }
 
-  private toBoolean(value: any): boolean{
-    switch(value){
+  private toBoolean(value: any): boolean {
+    switch (value) {
       case "1":
         return true;
       case "0":
-          return false;
+        return false;
       default:
         return undefined;
     }
+  }
+
+  private listChangedProperties(): [string, any][] {
+    const changed: [string, any][] = [];
+
+    if (this.dataForm.controls['temperatureSet'].value !== this.dataBackup.temperatureSet) {
+      changed.push([this.temperatureSetCode, this.dataForm.controls['temperatureSet'].value]);
+    }
+    if (this.dataForm.controls['fan'].value !== this.dataBackup.fan) {
+      changed.push([this.fanCode, this.dataForm.controls['fan'].value]);
+    }
+    if (this.dataForm.controls['cooling'].value !== this.dataBackup.cooling) {
+      changed.push([this.coolingCode, this.dataForm.controls['cooling'].value]);
+    }
+    if (this.dataForm.controls['defrosting'].value !== this.dataBackup.defrosting) {
+      changed.push([this.defrostingCode, this.dataForm.controls['defrosting'].value]);
+    }
+    if (this.dataForm.controls['alarm'].value !== this.dataBackup.alarm) {
+      changed.push([this.alarmCode, this.dataForm.controls['alarm'].value]);
+    }
+
+    return changed;
   }
 }
--- src/app/services/http-caller.service.ts
+++ src/app/services/http-caller.service.ts
@@ -1,6 +1,6 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { map } from 'rxjs/operators'
 
 @Injectable({
@@ -11,12 +11,34 @@ export class HttpCallerService {
   constructor(private http: HttpClient) {
   }
 
-  loadPage(pageAddress: string, page: string):Observable<string>{
+  loadPage(pageAddress: string, page: string): Observable<string> {
     const fullAddress = `assets/${page}`;
-    return this.http.get(fullAddress, { responseType: 'text', observe:'response' }, ).pipe(
+    return this.http.get(fullAddress, { responseType: 'text', observe: 'response' },).pipe(
       map(data => {
         return data.body;
       })
+    );
+  }
+
+  postChange(pageAddress: string, page: string, changes: [string, any][]): void {
+    const fullAddress = `${pageAddress}/${page}`;
+
+    let options = {
+      headers: new HttpHeaders().set('Content-Type', 'application/x-www-form-urlencoded')
+    };
+
+    let body = new URLSearchParams();
+    changes.forEach(element => {
+      body.set(element[0], element[1])
+    });
+    console.log(`posting body: ${body}`);
+    this.http.post(fullAddress, body.toString(), options).subscribe(
+      response => {
+        console.log(response);
+      },
+      err => {
+        console.error(err);
+      }
     );
   }
 }
